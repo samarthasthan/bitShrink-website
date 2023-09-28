76f3cadc7d486542ca6b479533285cd80dbcf1b0
Samarth Asthan <samarthasthan@Samarths-MacBook-Air.local>
2023-09-29 00:05:01 +0530

    favicon added

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,14 @@ import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
 import VideoSection from "./components/videoSection/VideoSection";
 import Footer from "./components/footer/Footer";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className={styles.app}>
+      <Head>
+        <title>bitShrink</title>
+      </Head>
       <Header></Header>
       <Hero></Hero>
       <VideoSection></VideoSection>
